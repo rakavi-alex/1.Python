0faced8e0dfcb095a4d10f6015dd4763c0d7ce62
rakavi-alex <ragaviashwana19@gmail.com>
2026-05-07 12:11:14 +0530

    Add files via upload

---
+++ Assignments/Multiplefunctions.py
@@ -0,0 +1,34 @@
+class multiplefunctions():
+def Subfields():
+    print("Sub-Fields in AI are :")
+    for temp in lists:
+     print(temp) 
+    
+def OddEven():
+    num=int(input("Enter the Number"))
+    if((num%2)==1):
+        print(num,"is Odd number")
+        message=" is Odd number"
+    else: 
+        print(num," is Even number")
+        message="is Even number"
+        
+def Eligible():
+        aage=int(input("Enter your Age"))
+        if (age>18):
+          print(" Is Eligible For Marriage")
+        else:
+          print(" Is Not Eligible for Marriage")
+                 
+                
+def percentage():
+        subject1=int(input("Subject1"))
+   subject2=int(input("Subject2"))
+   subject3=int(input("Subject3"))
+   subject4=int(input("Subject4"))
+   subject5=int(input("Subject5"))
+   total=subject1+subject2+subject3+subject4+subject5
+   percentage=(total/500)*100
+   print(total,"Total")
+   print(percentage,"Percentage")
+   
